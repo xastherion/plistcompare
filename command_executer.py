77a63dc9d9bd1502771c176af5ba63012b7fbe28
xastherion <xastherion@gmail.com>
2017-10-31 16:26:48 +0100

    commit

--- command_executer.py
+++ command_executer.py
@@ -1,19 +1,34 @@
-import sys,os
+import sys, os
 from tkinter import *
 
+mGui = Tk()
+ment = StringVar()
+
 def mhello():
-        mtext=ment.get()
-        mlabel=Label(mGui,text=mtext).grid(row=3,column=0)
-        os.system(mtext)
-        return
+    mtext = ment.get()
+    mlabel = Label(mGui, text=mtext).grid(row=3, column=0)
+    os.system(mtext)
+    return
 
-mGui=Tk()
-ment=StringVar()
+def one_command():
+   mlabel = Label(mGui, text="Comando:").grid(row=0, column=0)
+   mentry = Entry(mGui, textvariable=ment, width=100).grid(row=0, column=1)
+   mbutton = Button(mGui, text='Ejecutar', command=mhello).grid(row=0, column=7)
 
+def exit_command():
+    quit()
 
-mlabel=Label(mGui,text="Comando:").grid(row=0,column=0)
-mentry=Entry(mGui,textvariable=ment).grid(row=0,column=1)
-mbutton=Button(mGui,text='Ejecutar',command=mhello).grid(row=2,column=2)
+# Principal Commands
+one_command()
+one_command()
+
+add_button = Button(mGui, text='Add Comando',command=one_command()).grid(row=1, column=1)
+exit_button = Button(mGui, text='Exit',command=exit_command).grid(row=1, column=7)
+
+grid_label='grid(row=0, column=0)'
+grid_entry='grid(row=0, column=1)'
+grid_button='grid(row=0, column=7)'
+position=(grid_label, grid_entry, grid_button)
 
 
 # -------------------------------------------------------------------
