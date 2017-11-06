22ac7c324c0013faf40a48885c35a9725853c30e
oxoloxo <carlos.gonzalez@pcanda.de>
2017-11-06 20:00:10 +0100

    aufgeräumt

--- multi_command_executer.py
+++ multi_command_executer.py
@@ -9,74 +9,37 @@ lista_cmd = [
     'ifconfig',
     'echo "XXXXX - OOOOO - XXXXX"'
 ]
-# ---------------- End Defaults Commands -------------
+# ================ End Defaults Commands =====================
 
 
-ment_01 = StringVar(mGui, value=lista_cmd[0])
-ment_02 = StringVar(mGui, value=lista_cmd[1])
-ment_03 = StringVar(mGui, value=lista_cmd[2])
-
-
-# ----- Execution in System --------------
-def sys_command_01():
-    mtext = ment_01.get()
-    os.system(mtext)
-    return
-
-
-def sys_command_02():
-    mtext = ment_02.get()
-    os.system(mtext)
-    return
-
-
-def sys_command_03():
-    mtext = ment_03.get()
-    os.system(mtext)
-    return
-
-# ------ End Execution in System ----------
-
-# ------ Dialog Boxes building ------------
-
+# ---------------- Dialog Boxes building ------------
 def command_01():
-    Label(mGui, text="Comando_01:").pack()
-    Entry(mGui, textvariable=ment_01, width=100).pack()
-    Button(mGui, text='Ejecutar', command=sys_command_01).pack()
+    ment_01 = StringVar(mGui, value=lista_cmd[0])
+    Label(mGui, text='Primer Comando')
+    Entry(mGui, textvariable=ment_01, width=100).pack(fill=Y)
+    Button(mGui, text='Ejecutar', command=lambda : os.system(ment_01.get())).pack(fill=Y)
 
 
 def command_02():
-    Label(mGui, text="Comando_02:").pack()
-    Entry(mGui, textvariable=ment_02, width=100).pack()
-    Button(mGui, text='Ejecutar', command=sys_command_02).pack()
+    ment_02 = StringVar(mGui, value=lista_cmd[1])
+    Entry(mGui, textvariable=ment_02, width=100).pack(fill=Y)
+    Button(mGui, text='Ejecutar', command=lambda : os.system(ment_02.get())).pack(fill=Y)
 
 
 def command_03():
-    Label(mGui, bg='blue', text="Comando_03:").pack()
-    Entry(mGui, textvariable=ment_03, width=100).pack()
-    Button(mGui, text='Ejecutar', command=sys_command_03).pack()
+    ment_03 = StringVar(mGui, value=lista_cmd[2])
+    Entry(mGui, textvariable=ment_03, width=100).pack(fill=Y)
+    Button(mGui, text='Ejecutar', command=lambda : os.system(ment_03.get())).pack(fill=Y)
 
 
-# --- Button Exit ------
-def exit_command():
-    quit()
-# --- End Exit Button --
-
-
-# Principal Commands
+# Call to DialogBox  Principal Commands
 command_01()
 command_02()
 command_03()
-print(lista_cmd[0])
 
 # ---- Building Exit Button ------
-exit_button = Button(mGui, text='Exit', command=exit_command).pack()
+exit_button = Button(mGui, text='Exit', command=lambda : quit()).pack()
 # ---- End Building Exit Button --
 
-
-<<<<<<< HEAD
 # -------------------------------------------------------------------
-=======
-# --------------------------------------------------------------------
->>>>>>> origin/master
 mainloop()
